abd4870238b6116bb773689f41a7d8cc1b233030
lalit <lalitpatil25825@gmail.com>
2025-06-26 23:03:02 +0530

    fix: removed duplicate express declaration

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config();               // 1. load .env
 const mongoose = require('mongoose');     // 2. import mongoose
 mongoose.connect(process.env.MONGO_URL);  // 3. then connect
 
-const express = require('express');
+
 // ... rest of your code
 
 
